be2907ebec6ec82b8517147222bfb5ec30b0ba35
Tom Albers <toma@kde.org>
2007-11-25 20:16:46 +0000

    Relicense from GPL2 -> GPL2+, main author has approved, the remaining contributors had less than 3 loc. The existing license talked about 'library', so not sure if this shouldn't be LGPL2+. CCMAIL:marchand@kde.org
    svn path=/trunk/KDE/kdesdk/kioslave/; revision=741513

--- svn/ksvnd/ksvnd.h
+++ svn/ksvnd/ksvnd.h
@@ -3,9 +3,10 @@
 
     Copyright (C) 2003-2005 Mickael Marchand <marchand@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    version 2 as published by the Free Software Foundation.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
     This software is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
